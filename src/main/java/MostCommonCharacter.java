51b3d35b31ba0890281444a141463bd303f4d7c0
Mark Snyder <Snyder4000@yahoo.com>
2023-03-13 15:32:24 +0000

    Completed Lab MostCommonCharacter

--- src/main/java/MostCommonCharacter.java
+++ src/main/java/MostCommonCharacter.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class MostCommonCharacter {
     /**
      * Find the most common character in str.
@@ -8,6 +8,18 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        HashMap<Character, Integer> map = new HashMap<>();
+        int stringLen = str.length();
+        char answer = 0;
+        int count = 0;
+        for(int i = 0; i < stringLen; i++){
+            char c = str.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+            if(count < map.get(c)) {
+                answer = c;
+                count = map.get(c);
+            }
+        }
+        return answer;
     }
 }
